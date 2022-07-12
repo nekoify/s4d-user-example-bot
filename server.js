18f9379ee7fee60cee16185c426beca231fcaa78
Cat Soup <98920372+CatSoup1@users.noreply.github.com>
2022-07-12 21:05:22 +1000

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const server = express();
-
-server.all('/', (req, res)=>{
+const port = process.env.PORT || 3000;
+server.all('/', (req, res) => {
     res.send("hello Jeremy")
+    res.header("Access-Control-Allow-Origin", req.header('origin'));
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", "true");
 })
 
-function keepAlive(){
-    server.listen(3000, ()=>{console.log("webserver online")});
+function keepAlive() {
+    server.listen(port, () => { console.log("webserver online") });
 }
 module.exports = keepAlive;
