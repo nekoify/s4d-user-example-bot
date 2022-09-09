509741b1519ad7f1c2288ff6c93ee6693669c005
Cat Soup <xl83dev@gmail.com>
2022-09-09 10:32:25 +1000

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000
 server.all('/', (req, res)=>{
     res.send("sdfgghfg")
 })
-
+console.log(port)
 function keepAlive(){
     server.listen(port, ()=>{console.log("webserver online")});
 }
