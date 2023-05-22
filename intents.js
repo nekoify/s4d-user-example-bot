f7c958aec24fa8490d8a9ab0b4c576bed2c487a2
nekoify <xl83dev@gmail.com>
2023-05-22 10:55:11 +1000

    Update intents.js

--- intents.js
+++ intents.js
@@ -5,29 +5,16 @@ var intents =
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMembers,
         GatewayIntentBits.GuildBans,
-        GatewayIntentBits.GuildEmojisAndStickers,
         GatewayIntentBits.GuildIntegrations,
-        GatewayIntentBits.GuildWebhooks,
-        GatewayIntentBits.GuildInvites,
-        GatewayIntentBits.GuildVoiceStates,
-        GatewayIntentBits.GuildPresences,
         GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.GuildMessageReactions,
-        //GatewayIntentBits.GuildMessageTyping,
-        GatewayIntentBits.DirectMessages,
-        GatewayIntentBits.DirectMessageReactions,
-        //GatewayIntentBits.DirectMessageTyping,
-        GatewayIntentBits.MessageContent,
-        GatewayIntentBits.GuildScheduledEvents
+        GatewayIntentBits.GuildMessageReactions
     ],
     partials:[
         Partials.User,
         Partials.Channel,
         Partials.GuildMember,
         Partials.Message,
-        Partials.Reaction,
-        Partials.GuildScheduledEvent,
-        Partials.ThreadMember
+        Partials.Reaction
     ]
 }
 
